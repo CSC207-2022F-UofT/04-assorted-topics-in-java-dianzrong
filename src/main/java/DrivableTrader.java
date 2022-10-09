8edd0be30a925515f9b080a0958a2d57c431e6a0
Diian-r <dianrong2012@gmail.com>
2022-10-08 23:09:41 -0400

    Tried everything and tested on Intellij or whatevs

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,31 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable> {
+    /**
+     * Construct a DrivableTrader with an inventory, wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable item) {
+        int super_price = super.getSellingPrice(item);
+
+        if (super_price == Tradable.MISSING_PRICE) {
+            return super_price;
+        }
+
+        return super_price + item.getMaxSpeed();
+    }
+}
--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -10,6 +10,7 @@
  * Go through the TODOs below and complete them.
  */
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +40,11 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
+    public Trader(int money) {
+        this.money = money;
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+    }
 
 
 
@@ -47,6 +53,9 @@ public class Trader<T> {
     /* TODO: Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
+    public void addToWishlist(T newItem) {
+        this.wishlist.add(newItem);
+    }
 
 
 
@@ -59,6 +68,12 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
+    public int getSellingPrice(T item) {
+        if (item instanceof Tradable) {
+            return ((Tradable) item).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
